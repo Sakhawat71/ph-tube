d98876a48de2d34e757e71484c28134ccb3f6072
sakhawat71 <sakhawat.showrav@gmail.com>
2023-10-17 23:38:54 +0600

    error masage

--- app.js
+++ app.js
@@ -43,8 +43,19 @@ const getVideos = async (id) => {
 const displayVideos = (data) => {
 
     const videoContainer = document.getElementById("videos-container");
+    const errorMasege = document.getElementById("error")
     videoContainer.textContent = '';
+    errorMasege.textContent = '';
 
+    console.log(data)
+    if(data.status === false){
+        errorMasege.innerHTML = `
+        <div class="text-center mt-10">
+                <img class="mx-auto" src="img/Icon.png" alt="">
+                <p class="font-bold text-2xl mt-5">Oops!! Sorry, There is no content here</p>
+            </div>
+        `;
+    }
 
     data.data.forEach((video) => {
         const videoCart = document.createElement("div");
@@ -52,24 +63,29 @@ const displayVideos = (data) => {
         videoCart.innerHTML = `
         <div class="card card-compact bg-base-100 shadow-xl ">
                 <figure class="relative h-48"><img class="max-h-48 w-full h-full" src="${video.thumbnail}" />
-                <p class="absolute bg-black text-white  bottom-0 right-0 p-2 rounded-md ">${video.others?.posted_date ? video.others?.posted_date: ""}</p>
+                <p class="absolute bg-black text-white  bottom-0 right-0 rounded-md ">${video.others?.posted_date ? video.others?.posted_date: ""}</p>
                 </figure>
                 
                 <div class="card-body flex flex-row gap-3 h-32">
                     <div class="w-10 rounded-full">
                         <img class="max-w-10 max-h-10 rounded-full" src="${video.authors[0]?.profile_picture}" />
+                        
                     </div>
                     <div>
                         <h2 class="card-title font-bold">${video.title}</h2>
-                        <p>${video.authors[0].profile_name} ${video.authors[0]?.verified}</p>
+                        <p>${video.authors[0].profile_name} ${video.authors[0]?.verified}
                         <i class="fa-solid fa-certificate" style="color: #3008bf;"></i>
+                        </p>
+                        
                         <p>${video.others?.views} views</p>
                     </div>
                 </div>
         </div>
         `;
-        videoContainer.appendChild(videoCart)
-        console.log(video)
+        videoContainer.appendChild(videoCart);
+
+
+        
 
     })
 }
